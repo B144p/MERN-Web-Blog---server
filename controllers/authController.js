83daf765b6202bc064ceed9facce51f1ccd816d6
B144p <103378900+B144p@users.noreply.github.com>
2022-11-30 21:48:44 +0700

    change message when wrong password

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,7 +8,7 @@ exports.login = (req,res)=>{
         const token = jwt.sign({username},process.env.JWT_SECRET,{expiresIn:'30m'})
         return res.json({token,username})
     }else{
-        return res.status(400).json({error:'Wrong Password'})
+        return res.status(400).json({error:'Usernam or Password is invalid'})
     }
 }
 
